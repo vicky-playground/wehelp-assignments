9478a7f718d25c7840dabf4c43b2c372028c45cc
Vicky <90204593+vicky-playground@users.noreply.github.com>
2022-01-06 23:42:29 +0800

    Update HW.js

--- week-2/HW.js
+++ week-2/HW.js
@@ -47,7 +47,7 @@ function avg(data){
 
 
 // 3. using sorting approach
-function maxProduct(nums){  
+function maxProduct1(nums){  
     var arr1 = new Array();
     nums.sort(function( a , b){
         if(a > b) return 1;
@@ -65,7 +65,93 @@ function maxProduct(nums){
     } 
     console.log(ans);
 }  
+// 3. not using sorting approach
+function maxProduct(nums){  
+    if (nums.length<2){
+        console.log("The list is not in pairs");
+        return;
+    }
+     
+    var positive = [], negative = [];
+    for (var i = 0; i<nums.length;i++){
+        if (nums[i] >= 0){
+            positive.push(nums[i]);
+        }
+        else{
+            negative.push(nums[i]);
+        }
+    }
+    
 
+    if (positive.length === 0) {
+        var num3 = negative[0], num4 = 1;
+        for (var i = 0; i<negative.length;i++){
+            var temp1 = Math.max(num3, num4);
+            var temp2 = Math.min(num3, num4);
+            if (negative[i]<temp1){
+                num3 = temp2;
+                num4 = negative[i];
+            }
+            
+        }
+        
+        console.log(num3*num4);
+        return;
+    }
+     
+    else if (negative.length === 0){
+        var num3 = positive[0], num4 = 1;
+        for (var i = 0;i<positive.length;i++){
+            var temp1 = Math.max(num3, num4);
+            var temp2 = Math.min(num3, num4);
+            if (positive[i]>temp2){
+                num3 = positive[i];
+                num4 = temp1;
+            }      
+        }      
+        console.log(num3*num4);
+        return;
+    }
+     
+    else if (nums.length === 2){
+        console.log(nums[0]*nums[1]);
+        return;
+    }
+    
+    else{
+        var num1 = positive[0], num2 = 0, num3 = negative[0], num4 = 0;
+        for (var i=0; i<positive.length-1;i++){
+            var temp1 = Math.max(num1, num2);
+            var temp2 = Math.min(num1, num2);
+            if (positive[i+1]>temp2){
+                num1 = positive[i+1];
+                num2 = temp1;
+            }  
+            
+        }
+        
+        var ans1 = num1*num2
+        for (var i= 0 ;i<negative.length-1;i++){
+            var temp1 = Math.max(num3, num4);
+            var temp2 = Math.min(num3, num4);
+            if (negative[i+1]<temp1){
+                num3 = temp2;
+                num4 = negative[i+1];
+            }
+             
+        }
+        
+        var ans2 = num3*num4;
+        if (ans1 > ans2){
+            console.log(ans1);
+        }
+        
+        else{
+            console.log(ans2);
+        }
+           
+    }
+}
     maxProduct([5, 20, 2, 6]) // 得到 120 
     maxProduct([10, -20, 0, 3]) // 得到 30 
     maxProduct([-1, 2]) // 得到 -2 

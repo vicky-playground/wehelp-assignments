6b97c807e687c0b0171f7b8e175703209f1f6907
vicky-playground <vicky.kuo.contact+1@gmail.com>
2022-02-10 00:31:19 +0800

    revised session

--- week-6/main.py
+++ week-6/main.py
@@ -34,7 +34,8 @@ def signin():
         
     # If user doesn't exist in member table 
     if user is None:
-        session.pop('_flashes', None)
+        # make sure session is empty
+        session.clear()
         flash('帳號或密碼輸入錯誤')
         #Redirect to error page
         return redirect(url_for('error',message= "帳號或密碼輸入錯誤"))
@@ -61,7 +62,6 @@ def member():
 # http://127.0.0.1:3000/error/
 @app.route('/error/', methods=['GET', 'POST'])
 def error():
-    session.pop('_flashes', None)
     return render_template("error.html")
 
 # http://127.0.0.1:3000/signout
@@ -104,13 +104,18 @@ def signup():
         sql = "INSERT INTO member(name,username, password) VALUES (%s,%s,%s)"
         cursor.execute(sql, (name, username, password))
         flash('會員註冊成功')
+        # make sure session is empty
+        session.clear()
+
     # user exists in member table 
     elif len(user) > 0:
-        session.pop('_flashes', None)
+        # make sure session is empty
+        session.clear()
         flash('帳號已經被註冊')
         #Redirect to error page
         return redirect(url_for('error',message= "帳號已經被註冊"))
-    return render_template('index.html') 
+    
+    return redirect(url_for("login")) 
 
 
 # http://127.0.0.1:3000/
